93eacbfe49bef457a5b6ef95fa0e6a53c7246108
Chia <star7961@gmail.com>
2022-09-14 22:28:26 +0800

    first deploy

---
+++ docs/.vitepress/config.js
@@ -0,0 +1,12 @@
+/**
+ * @type {import('vitepress').UserConfig}
+ */
+ const config = {
+    base: '/blog',  // 儲存庫名稱
+    lang: 'zh-Hant-TW',
+    title: 'VitePress',
+    description: 'Just playing around.'
+  }
+  
+  export default config
+  
